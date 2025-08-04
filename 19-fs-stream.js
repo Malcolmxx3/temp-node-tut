a0b4cab5bbef0367d0951dd8b854c97c0fc0cc73
Adam Razak <adamrazak@Adams-MacBook-Pro.local>
2025-08-04 22:02:08 +0000

    second commit

---
+++ 19-fs-stream.js
@@ -0,0 +1,20 @@
+const { createReadStream } = require('fs')
+const { keyBy } = require('lodash')
+
+const stream = createReadStream('./content/big.txt', {
+  highWaterMark: 90000
+})
+
+stream.on('data', (result) => {
+  console.log(result)
+})
+
+stream.on('error', (err) => {
+  console.log(err)
+})
+
+// default 64kb
+// last buffer - remainder
+// highWaterMark - control size
+// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
+// const stream = createReadStream('./content/big.txt', { encoding: 'utf8' })
